44979a7f70d4e74ea90b6df64ad509c11fa88364
liernilekeitiobhi <lierniortiz@lekeitiobhi.net>
2022-10-07 13:13:54 +0200

    gh gehitu eta kopiatu botoia sartu funtzioetan

--- Matematika/js/marraztu.js
+++ Matematika/js/marraztu.js
@@ -263,5 +263,32 @@ function draw(){
     
 }
 
+function kopiatu(){
+    //Grafikoa dagoen div identifikatu
+    //div = document.getElementById("grafikoa")
+
+    var domNode = document.getElementById('grafikoa');
+	
+	// copy the canvas to the clipboard with chrome's CliboardItem API
+	// https://developers.google.com/web/updates/2019/07/image-support-for-async-clipboard#images
+	html2canvas(domNode).then(function(canvas) {
+		canvas.toBlob(function(blob) {
+			navigator.clipboard
+				.write([
+				new ClipboardItem(
+					Object.defineProperty({}, blob.type, {
+						value: blob,
+						enumerable: true
+					})
+				)
+			])
+				.then(function() {
+				alert("Kopiatu da!");
+			});
+		});
+    })
+    
+}
+
 
 
