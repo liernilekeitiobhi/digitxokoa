83da634cabd2c6ea3b7b386e2c2e179e65ddbc84
liernilekeitiobhi <lierniortiz@lekeitiobhi.net>
2024-02-02 12:01:28 +0100

    grafikoa fikin proportzional jarri

--- Fisika-Kimika/js/grafikoak.js
+++ Fisika-Kimika/js/grafikoak.js
@@ -3,38 +3,40 @@ let myChart
 let cfg = {
   type: 'line',
   data: {
-    labels: [0],
     datasets: [{
       label: 'Higikaria',
-      data: [0],
+      data: [],
       borderWidth: 1,
       fill: false,
       borderColor: 'rgb(75, 192, 192)',
       tension: 0,
-    }]
-    
+    }],
   },
   options: {
     scales: {
-      x: {axis: "x",
-          beginAtZero: true,
-          id: "",
-          title:{display: true, text:"x-ardatza"}},
-      y: {axis: "y",
-          beginAtZero: true,
-          id: "",
-          title:{display: true, text:"y-ardatza"}
-    }  
-    
+      x: {
+        type: 'linear',
+        position: 'bottom',
+        beginAtZero: true,
+        title: { display: true, text: "x-ardatza" },
+        ticks: {
+          min: 0,
+          max: 10, // Puedes ajustar este valor según tus necesidades
+          stepSize: 2 // Puedes ajustar este valor según tus necesidades
+        }
+      },
+      y: {
+        beginAtZero: true,
+        title: { display: true, text: "y-ardatza" }
+      }
+    }
   }
-}
-}
+};
 
 
 function addPoints(id){
   console.log("id"+id)
-  xArray = cfg.data.labels
-  yArray = cfg.data.datasets[0].data
+  dataArray = cfg.data.datasets[0].data
   if (id=="x-ardatza"){
     xArdatza = document.getElementById('x-ardatza').value
     cfg.options.scales.x.title.text = xArdatza
@@ -46,33 +48,20 @@ function addPoints(id){
     cfg.options.scales.y.title.text = yArdatza
     console.log(yArdatza)
   }
-  if (id=="1"){
+  if (id=="1" || id=="2" || id=="3" || id=="4" || id=="5"){
     x = document.getElementById('x'+id).value;
     y = document.getElementById('y'+id).value;
-    console.log(x)
-    if (x=="0" && y!="0"){
-      yArray[0] = y
-    }
-    if (x!="0"){
-      alert('0 aldiunean zer gertatzen den zehaztu behar da!')
-    }
+    dataArray.push({'x':x, 'y':y})
 
   }
-  if (id=="2" || id=="3" || id=="4" || id=="5"){
-    x = document.getElementById('x'+id).value;
-    y = document.getElementById('y'+id).value;
-    xArray.push(x)
-    yArray.push(y)
-
-  }
-  cfg.data.labels = xArray
-  cfg.data.datasets[0].data = yArray
+  cfg.data.datasets[0].data = dataArray
 
   if (myChart) {
     myChart.destroy();
   }
   const ctx = document.getElementById('myChart');
   myChart = new Chart(ctx, cfg);
+  console.log(cfg)
 
   
 }
